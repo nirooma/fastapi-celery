0e8e48036a5e1a8d3fae2d47df578f3ff99477ad
nirmansour <nirooma@icloud.com>
2021-09-04 15:59:44 +0300

    revert commit

--- project/__init__.py
+++ project/__init__.py
@@ -48,7 +48,8 @@ def create_app() -> FastAPI:
                 "container": os.uname()[1],
                 "path": request.scope.get("path"),
                 "environment": settings.ENVIRONMENT,
-                "debugMode": settings.DEBUG
+                "debugMode": settings.DEBUG,
+                "try": 1
             }
         )
 
